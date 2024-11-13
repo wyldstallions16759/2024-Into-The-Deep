788528e6e4e5d470c105585774b6f46d3d9020d3
wyldstallions16759 <wyldstallions16759@gmail.com>
2024-11-13 16:09:11 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSubsystem.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ArmSubsystem {
+
+    // Constructor
+    public ArmSubsystem(HardwareMap hwMap, Telemetry telemetry) {
+
+        // Initialize motors
+    }
+
+
+    // Move arm up until the newPosition reached
+    public boolean armUp(int newPosition) {
+
+    }
+
+    // Move arm down until newPosition reached
+    public boolean armDown(int newPosition) {
+
+    }
+
+    // Extend arm out until the newPosition reached
+    public boolean armExtend(int newPosition) {
+
+    }
+
+    // Extend arm out until the newPosition reached
+    public boolean armRetract(int newPosition) {
+
+    }
+
+    public boolean fingersOpen() {
+
+    }
+
+    public boolean fingersClose() {
+
+    }
+}
